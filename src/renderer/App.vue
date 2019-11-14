b9c6d8541e2255155224b79efb0a3166180d116c
Yura Rekechuk <okgo0801@gmail.com>
2019-11-14 11:07:37 +0200

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  #app
+    router-view
+  //- <div id="app">
+  //-   <router-view></router-view>
+  //- </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-app'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
